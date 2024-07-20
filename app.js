e0eb7a881e44833c19f15b5886b4dc9ffacd768f
Muhammad Huzaifa <huzaifa2008khi@gmail.com>
2024-07-20 14:33:53 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,81 @@
+var num1 = +prompt("Enter Table Number")
+var num2 = +prompt("Table kahan tak")
+
+
+for(i=1 ; i<=num2 ; i++)
+{document.write(num1 +" "+ "x"+" " + i +" " + "=" + " " + num1*i + "<br>")}
+
+
+
+// var fruits = [ "Apple","Banana","Pineapple","Grapes","peach"]
+// for (var i = 0 ; i < fruits.length ; i++){
+//     document.write("Elements at index " + i + " is " + fruits[i] + "<br>")
+// }   
+// document.write("<br>")
+
+// for (var i = 1 ; i < 15 ; i++){
+//     document.write(i +",")
+// }
+// document.write("<br>")
+
+// for (var i = 10 ; i > 0 ; i--){
+//     document.write(i +",")
+// }
+// document.write("<br>")
+
+// for (var i = 1 ; i <= 20 ;i++){
+//     if(i % 2 == 0){document.write(i + ",")}
+// }
+// document.write("<br>")
+
+// for (var i = 1 ; i <= 20 ;i++){
+//     if(i % 2 == 1){document.write(i + ",")}
+// }
+
+// document.write("<br>")
+
+// for (var i = 1 ; i <= 20 ;i++){
+//     if(i % 2 == 0){document.write(i + "k,")}
+// }
+
+
+// var userinput = +prompt("Value")
+
+// for(
+//     var i = 0 ; i < userinput ; i++
+// ){
+
+//     var q = prompt("enter")
+//     document.write(q + "<br>")
+
+// }
+
+// for(
+//     var i = 1 ; i  <= 100;i = i + 10
+//     ){
+//     for (var j =1 ;j < i + 10 ; j++) {
+//         document.write(o + " " )
+//     }
+    
+//     document.write("<br>")
+// }
+
+
+
+
+// document.write("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
